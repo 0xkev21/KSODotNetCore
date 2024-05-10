ef9ec3c9d7142d64cddc17497bd1fdf4d8c84398
Kev <87592672+0xkev21@users.noreply.github.com>
2024-05-10 21:20:56 +0630

    [Complete] SnakeController

--- KSODotNetCore.ConsoneAppHttpClient/Program.cs
+++ KSODotNetCore.ConsoneAppHttpClient/Program.cs
@@ -2,12 +2,20 @@
 
 Console.WriteLine("Hello, World!");
 
-string jsonStr = await File.ReadAllTextAsync("data.json");
-var model = JsonConvert.DeserializeObject<MainDto>(jsonStr);
+//string jsonStr = await File.ReadAllTextAsync("data.json");
+//var model = JsonConvert.DeserializeObject<MainDto>(jsonStr);
 
-foreach(var question in model.questions)
+//foreach(var question in model.questions)
+//{
+//    Console.WriteLine(question.questionNo);
+//}
+
+string jsonStr = await File.ReadAllTextAsync("Snakes.json");
+var snakesModel = JsonConvert.DeserializeObject<SnakesModel>(jsonStr);
+
+foreach (var snake in snakesModel.snakes)
 {
-    Console.WriteLine(question.questionNo);
+    Console.WriteLine(snake.Id);
 }
 
 // Json to C#
@@ -35,3 +43,21 @@ public class Answer
     public int answerNo { get; set; }
     public string answerResult { get; set; }
 }
+
+
+
+public class SnakesModel
+{
+    public Snake[] snakes { get; set; }
+}
+
+public class Snake
+{
+    public int Id { get; set; }
+    public string MMName { get; set; }
+    public string EngName { get; set; }
+    public string Detail { get; set; }
+    public string IsPoison { get; set; }
+    public string IsDanger { get; set; }
+}
+
---
+++ KSODotNetCore.RestApiWithNLayer/Features/Snakes/SnakesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace KSODotNetCore.RestApiWithNLayer.Features.Snakes
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SnakesController : ControllerBase
+    {
+        private  SnakesModel _data;
+        
+        private async Task<SnakesModel>GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("Snakes.json");
+            var model = JsonConvert.DeserializeObject<SnakesModel>(jsonStr);
+            return model;
+        }
+
+        [HttpGet("snakes")]
+        public async Task<IActionResult> Snakes()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.snakes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Snake(int id)
+        {
+            var model = await GetDataAsync();
+            var snake = model.snakes.FirstOrDefault(x => x.Id == id);
+            if (snake is null) return NotFound("No data found");
+            return Ok(snake);
+        }
+    }
+}
+
+public class SnakesModel
+{
+    public Snake[] snakes { get; set; }
+}
+
+public class Snake
+{
+    public int Id { get; set; }
+    public string MMName { get; set; }
+    public string EngName { get; set; }
+    public string Detail { get; set; }
+    public string IsPoison { get; set; }
+    public string IsDanger { get; set; }
+}
