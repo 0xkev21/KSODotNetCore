08454cbe96758d802f6818894917b7ebb799349d
Kev <87592672+0xkev21@users.noreply.github.com>
2024-06-10 22:52:30 +0630

    [Complete] jQuery Plugin (SweetAlert, Notiflix)

--- KSODotNetCore.HTML/blog.js
+++ KSODotNetCore.HTML/blog.js
@@ -9,17 +9,17 @@ function readBlog() {
 // Edit Function
 function editBlog(id) {
   const list = getBlogs();
-  const item = list.find(x => x.id === id);
-  if(item === undefined) {
+  const item = list.find((x) => x.id === id);
+  if (item === undefined) {
     console.log("No data found");
     errorMessage("No data found");
     return;
   }
 
-  $('#txtTitle').val(item.title);
-  $('#txtAuthor').val(item.author);
-  $('#txtContent').val(item.content);
-  $('#txtTitle').focus();
+  $("#txtTitle").val(item.title);
+  $("#txtAuthor").val(item.author);
+  $("#txtContent").val(item.content);
+  $("#txtTitle").focus();
 
   currBlogId = item.id;
 }
@@ -31,8 +31,8 @@ function createBlog(title, author, content) {
     id: uuidv4(),
     title: title,
     author: author,
-    content: content
-  }
+    content: content,
+  };
 
   list.push(requestModel);
 
@@ -47,8 +47,8 @@ function createBlog(title, author, content) {
 // Update Function
 function updateBlog(id, title, author, content) {
   const list = getBlogs();
-  const item = list.find(x => x.id === id);
-  if(item === undefined) {
+  const item = list.find((x) => x.id === id);
+  if (item === undefined) {
     console.log("No data found");
     errorMessage("No data found");
     return;
@@ -68,84 +68,107 @@ function updateBlog(id, title, author, content) {
 
 // Delete Function
 function deleteBlog(id) {
-  let result = confirm("Are you sure to delete?");
-  if(!result) return;
-  
-  let list = getBlogs();
+  Notiflix.Confirm.show(
+    "Confirm Delete",
+    "Are you sure you want to delete?",
+    "Delete",
+    "Cancel",
+    () => {
+      let list = getBlogs();
 
-  const item = list.find(x => x.id === id);
-  if(item === undefined) {
-    console.log("No data found");
-    errorMessage("No data found");
-    return;
-  }
+      const item = list.find((x) => x.id === id);
+      if (item === undefined) {
+        console.log("No data found");
+        errorMessage("No data found");
+        return;
+      }
 
-  list = list.filter(x => x.id !== id);
+      list = list.filter((x) => x.id !== id);
 
-  const jsonBlogs = JSON.stringify(list);
-  localStorage.setItem(tblBlogs, jsonBlogs);
+      const jsonBlogs = JSON.stringify(list);
+      localStorage.setItem(tblBlogs, jsonBlogs);
 
-  successMessage("Deleting Successful");
-  getBlogTable();
-  clearControls();
+      successMessage("Deleting Successful");
+      getBlogTable();
+      clearControls();
+    },
+    () => {
+      return;
+    },
+    { okButtonBackground: 'red'},
+  );
 }
 
 // random uuid generator
 function uuidv4() {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+    (
+      +c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
+    ).toString(16),
   );
 }
 
 // Get Blogs from localStorage function
 function getBlogs() {
   const blogs = localStorage.getItem(tblBlogs);
-  const list = (blogs === null) ? [] : JSON.parse(blogs);
+  const list = blogs === null ? [] : JSON.parse(blogs);
   return list;
 }
 
-
 // Click Events
-$('#btnSave').click(function() {
-  const title = $('#txtTitle').val();
-  const author = $('#txtAuthor').val();
-  const content = $('#txtContent').val();
+$("#btnSave").click(function () {
+  const title = $("#txtTitle").val();
+  const author = $("#txtAuthor").val();
+  const content = $("#txtContent").val();
 
-  if(currBlogId === null) {
+  if (currBlogId === null) {
     createBlog(title, author, content);
   } else {
     updateBlog(currBlogId, title, author, content);
     blogId = null;
   }
-})
+});
 
 // Messages
 function successMessage(message) {
-  alert(message);
+  Swal.fire({
+    title: "Success!",
+    text: message,
+    icon: "success",
+  });
 }
 function errorMessage(message) {
-  alert(message);
+  Swal.fire({
+    title: "Error!",
+    text: message,
+    icon: "error",
+  });
 }
 
 // Form Controls reset
 function clearControls() {
-  $('#txtTitle').val('');
-  $('#txtAuthor').val('');
-  $('#txtContent').val('');
-  $('#txtTitle').focus();
+  $("#txtTitle").val("");
+  $("#txtAuthor").val("");
+  $("#txtContent").val("");
+  $("#txtTitle").focus();
 }
 
 // For Table Refreshing
 function getBlogTable() {
   const list = getBlogs();
   let count = 0;
-  let htmlRows = '';
-  list.forEach(item => {
+  let htmlRows = "";
+  list.forEach((item) => {
     const htmlRow = `
     <tr>
       <td>
-      <button type="button" class="btn btn-warning" onclick="editBlog('${item.id}')">Edit</button>
-      <button type="button" class="btn btn-danger" onclick="deleteBlog('${item.id}')">Delete</button>
+      <button type="button" class="btn btn-warning" onclick="editBlog('${
+        item.id
+      }')">Edit</button>
+      <button type="button" class="btn btn-danger" onclick="deleteBlog('${
+        item.id
+      }')">Delete</button>
       </td>
       <td>${++count}</td>
       <td>${item.title}</td>
@@ -153,10 +176,10 @@ function getBlogTable() {
       <td>${item.content}</td>
     </tr>
   `;
-  htmlRows += htmlRow;
+    htmlRows += htmlRow;
   });
 
-  $('#tbodyBlogs').html(htmlRows);
+  $("#tbodyBlogs").html(htmlRows);
 }
 
 // Initializations
